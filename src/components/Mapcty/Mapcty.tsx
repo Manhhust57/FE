6a6ce5c3ef11bbaca659bda49dfeb7d967b56567
Manhhust57 <quocmanh572005@gmail.com>
2025-09-18 14:34:13 +0700

    update book room

--- src/components/Mapcty/Mapcty.tsx
+++ src/components/Mapcty/Mapcty.tsx
@@ -4,7 +4,7 @@ const Mapcty = () => {
   return (
     <div className="map-cty-main">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.502480086302!2d105.79025815030758!3d21.052584022806705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab1a4c13caab%3A0xeacc95be8bf20882!2s901A%20Starlake!5e0!3m2!1svi!2s!4v1741919002451!5m2!1svi!2s"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.9798910304207!2d107.00014641103532!3d20.953322680596486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a5fbce3525b65%3A0xeb9eed19f0f52778!2sAnstay%20Residence%20by%20A%20La%20Carte%20H%E1%BA%A1%20Long!5e0!3m2!1svi!2s!4v1758160741455!5m2!1svi!2s"
         width="1920px"
         height="500px"
         style={{ border: 0 }}
